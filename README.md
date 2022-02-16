4e1dcc3afc495c48f9b1fe1f79bf7646ca8283a7
jonty <jontyzhen@gmail.com>
2022-02-16 23:01:05 +0800

    day2: 安头像

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <!--
  * @Author: jonty
  * @Date: 2022-02-15 20:00:38
- * @LastEditTime: 2022-02-15 20:34:26
+ * @LastEditTime: 2022-02-16 22:20:12
  * @Description: 
  * @Reference: 
  * @FilePath: \in100days\README.md
@@ -21,3 +21,20 @@
 # 布局
 
 从 antd 里考一个布局贴成一个组件
+
+# git
+1.远程github先建个仓库
+2.本地git init
+3.修改.gitignore 过滤掉没必要的文件比如 node_module
+4.git commit -m 'xxx'
+5.git remote add [shortname] [url] 
+> url便是github上的.git后缀的仓库地址 与远程仓库连接上才能push
+> origin 是远程仓库的别名
+> eg: 显示远程仓库的信息：git remote show [shortname]
+6.git push -u origin master 
+（这些在github建了一个新仓库后都会有的 菜鸟教程上有一些冷知识）
+
+# 安路由
+```sh
+npm install react-router-dom@6
+```
--- src/pages/iLayout.js
+++ src/pages/iLayout.js
@@ -1,20 +1,33 @@
 /*
  * @Author: jonty
  * @Date: 2022-02-15 20:17:55
- * @LastEditTime: 2022-02-15 21:23:20
+ * @LastEditTime: 2022-02-16 22:16:06
  * @Description: 
  * @Reference: 
  * @FilePath: \in100days\src\pages\iLayout.js
  */
 import React from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { Layout, Menu, Breadcrumb, Avatar, Dropdown, message } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined, DownOutlined } from '@ant-design/icons';
 import './iLayout.css' // 引入一个css
 
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
-export function ILayout() {
+export function ILayout(props) {
+
+    const onClick = ({ key }) => {
+        message.info(`Click on item ${key}`);
+    };
+
+    const menu = (
+        <Menu onClick={onClick}>
+            <Menu.Item key="1">1st menu item</Menu.Item>
+            <Menu.Item key="2">2nd menu item</Menu.Item>
+            <Menu.Item key="3">3rd menu item</Menu.Item>
+        </Menu>
+    )
+
     return (
         <Layout >
             {/* 头部区域 */}
@@ -22,13 +35,19 @@ export function ILayout() {
                 <div className="logo" style={{ float: 'left' }}> {/* logo块左浮 让菜单上去 */}
                     <span style={{ color: 'white' }}>LOGO</span>
                 </div>
-                <div>
-                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} className='headerMenu'>
-                        <Menu.Item key="1">nav x</Menu.Item>
-                        <Menu.Item key="2">nav 2</Menu.Item>
-                        <Menu.Item key="3">nav 3</Menu.Item>
-                    </Menu>
-                </div>
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} className='headerMenu'>
+                    <Menu.Item key="1">nav x</Menu.Item>
+                    <Menu.Item key="2">nav 2</Menu.Item>
+                    <Menu.Item key="3">nav 3</Menu.Item>
+                </Menu>
+                {/* 头像区域 */}
+                <Dropdown overlay={menu} className='avatar'>
+                    <div>
+                        <Avatar>U</Avatar>
+                        <span className='avatarName'>用户名</span>
+                    </div>
+                </Dropdown>
+
             </Header>
 
             <Layout style={{ height: '100%' }}>
@@ -37,6 +56,7 @@ export function ILayout() {
                 <Sider className="site-layout-background">
                     <Menu
                         mode="inline"
+                        theme='dark'
                         defaultSelectedKeys={['1']}
                         defaultOpenKeys={['sub1']}
                         style={{ height: '100%', borderRight: 0 }}
@@ -79,6 +99,7 @@ export function ILayout() {
                         }}
                     >
                         Content
+                        {props.children}
                     </Content>
                 </Layout>
 
